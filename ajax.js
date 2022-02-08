1c5f3b205aae93f3c218bf11a9079fd0629f5507
Young-Einstein10 <aabdulrahmanyusuf@gmail.com>
2022-02-08 13:04:50 +0100

    Learning AJAX

--- ajax.js
+++ ajax.js
@@ -10,14 +10,22 @@ Take NOte of the following:-
     - AJAX is not a programming language.
     - AJAX is a technique for accessing web servers from a web page.
     - AJAX stands for Asynchronous JavaScript And XML.
+
+
+    Example: We're trying to find the average of 4 numbers
+
+    Add all numbers togeter
+    Divide the sum of all the number by the number of time each number occurs
+
+    10, 12, 20, 15
+
+    10 + 12 + 20 + 15 = 57 
+
+    57 / 4
 */
 
-var request = new XMLHttpRequest();
-
-// GET
-// POST
-// PUT
-// DELETE
+// Making a GET Request
+const request = new XMLHttpRequest();
 
 request.open("GET", "https://jsonplaceholder.typicode.com/todos");
 
@@ -41,4 +49,35 @@ request.onreadystatechange = function () {
   }
 };
 
-request.send();
+// request.send();
+
+const btnMakeRequest = document.querySelector(".btn-make-request");
+
+// Making a POST Request
+function makePostRequest() {
+  request.open("POST", "https://jsonplaceholder.typicode.com/posts");
+
+  const dataToSend = JSON.stringify({
+    title: "Learning AvascripJAX",
+    body: "This post is about learning how AJAX works in Javascript",
+    userId: 4,
+  });
+
+  request.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status >= 200) {
+      const results = JSON.parse(this.response);
+
+      console.log(results);
+    }
+
+    // if (this.status !== 200) {
+    //   alert("There was an error fetching this data");
+    // }
+  };
+
+  request.send(dataToSend);
+}
+
+btnMakeRequest.addEventListener("click", makePostRequest);
